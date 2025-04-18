9b83dfa485f93a6a362aa218ab783daef66ee217
Abhiram-Mangde <abhirammangde0@gmail.com>
2025-04-18 11:54:13 +0530

    Add webinspect script

---
+++ run_webinspect.bat
@@ -0,0 +1,2 @@
+REM Replace with correct path and parameters
+"C:\Program Files\HP\HP WebInspect\WebInspect.exe" /scan /url http://localhost:3000 /policy Default /report webinspect_output/report.xml
